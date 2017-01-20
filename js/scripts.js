3960bb516b70b87b45de30acb0434c656c58d5b8
postolenko <denis.postolenko@yandex.ua>
2017-01-20 13:01:40 +0200

    Show Travel

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ $(document).ready(function() {
 
     getPopupPosition();
 
+    getServicesLeftColBg();
+
     $(window).resize(function() {
 
 
@@ -24,7 +26,13 @@ $(document).ready(function() {
 
         // ----------------------------------------------------------------------------
 
+        $(".services-white-bg").css({"width" : ( $(".services .col-1").offset().left + $(".services .col-1").width() ) + "px"});
+
+        // ----------------------------------------------------------------------------
+
         getPopupPosition();
+
+        getServicesLeftColBg();
 
     });
 
@@ -37,6 +45,33 @@ $(document).ready(function() {
         $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
 
     });
+
+    // ----------------------------------------------------------------------------------
+
+    var countCircleTooltip = $(".circle-tooltip").length - 1;
+
+    // console.log(countCircleTooltip);
+
+    var indexCircleTooltip = 0;
+
+    var circleTooltipContentHeight;
+
+    // for( indexCircleTooltip = 0; indexCircleTooltip <= countCircleTooltip; countCircleTooltip++ ) {
+
+        circleTooltipContentHeight = $(".circle-tooltip:eq("+ indexCircleTooltip +") .tooltip-content").height();
+
+        $(".circle-tooltip:eq("+ indexCircleTooltip +") .tooltip-content").css({
+                                                                                "height" : circleTooltipContentHeight + "px",
+                                                                                "top" : 145 / 2 + "px"
+                                                                            });
+
+        $(".circle-tooltip:eq("+ indexCircleTooltip +")").css({
+                                                "height" : circleTooltipContentHeight + 145 + "px",
+                                                "width" : circleTooltipContentHeight + 145 + "px"
+                                            });
+
+    // }
+
 
     // ----------------------------------------------------------------------------------
 
@@ -80,6 +115,58 @@ $(document).ready(function() {
 
     // ----------------------------------------------------------------------------------
 
+    $(function() {
+
+        var activeLinkAttr;
+
+        var linkAttr;
+
+        var serviceLinkCount = $(".service-link").length - 1;
+
+        var serviceLinkIndex = 0;
+
+        for(serviceLinkIndex = 0; serviceLinkIndex <= serviceLinkCount; serviceLinkIndex++) {
+
+            if( $(".service-link:eq(" + serviceLinkIndex + ")").hasClass("active") ) {
+
+                $(".service-content").css({"display" : "none"});
+
+                activeLinkAttr = $(".service-link:eq(" + serviceLinkIndex + ")").attr("data-link");
+
+                $('[data-service = "content_' + activeLinkAttr + '"]').addClass("active");
+
+                $('[data-service = "content_' + activeLinkAttr + '"]').css({"display" : "block"});
+
+            }
+
+        }
+
+
+        $(".service-link").click(function(serviceLink) {
+
+            serviceLink.preventDefault();
+
+            linkAttr = $(this).attr("data-link");
+
+            if( $(".service-content:visible").attr("data-service") != "content_" + linkAttr ) {
+
+                $(".service-content:visible").css({"display" : "none"});
+
+                $(".service-link").removeClass("active");
+
+                $('[data-service = "content_'+ linkAttr + '"]').css({"display" : "block"});
+
+                $(this).addClass("active");
+
+            }
+
+        });
+
+    });
+
+
+    // ----------------------------------------------------------------------------------
+
 
     function getPopupPosition() {
 
@@ -89,6 +176,23 @@ $(document).ready(function() {
         $(".popup-position").css({  "top" :  ( windowHeight - $(".popup-position").outerHeight(true) ) / 2 + "px",
                                     "left" : ( windowWidth - $(".popup-position").outerWidth(true) ) / 2 + "px" 
                                 });
+
+    }
+
+
+    // -----------------------------------------------------------------------------------
+
+    function getServicesLeftColBg() {
+
+        if( $(".services").length > 0 && $(".services .col-1 .services-white-bg").length <= 0 ) {
+
+            $(".services .col-1").css({"min-height" : $(".services").height() + "px"})
+
+            $(".services .col-1").prepend("<div class='services-white-bg'></div>");
+
+            $(".services-white-bg").css({"width" : ( $(".services .col-1").offset().left + $(".services .col-1").width() ) + "px"});
+
+        }
 
     }
 
